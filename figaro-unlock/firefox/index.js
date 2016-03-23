e909ae1807e02e618d13a3b18c17aac0533b85e0
Martin Smeeckaert <smeeckaert@novius.fr>
2016-03-23 18:24:09 +0100

    firefox permissions

--- figaro-unlock/firefox/index.js
+++ figaro-unlock/firefox/index.js
@@ -2,7 +2,13 @@ var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
 
 pageMod.PageMod({
-    include: "*",
+    include: [
+        "*.lefigaro.fr",
+        "*.lepoint.fr",
+        "*.marieclaire.fr",
+        "*.lequipe.fr",
+        "*.lexpress.fr",
+    ],
     contentStyleFile: data.url("style/content.css"),
     contentScriptWhen: "start",
     contentScriptFile: data.url("script/content.js")
