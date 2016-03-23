37a7b755b474eb595e85ce501daf18fa7ca10985
Martin Smeeckaert <smeeckaert@novius.fr>
2016-03-23 18:02:56 +0100

    lequipe

--- figaro-unlock/data/script/content.js
+++ figaro-unlock/data/script/content.js
@@ -1,8 +1,33 @@
-elems = document.querySelectorAll('.adBlockNotificationOverlay,.adBlockNotification');
-if (elems && elems.length) {
-    for (i = 0; i < elems.length; i++) {
-        elems[i].style = '';
+window.Ads = {};
+window.Ads.display = {};
+
+function cleanAds() {
+    elems = document.querySelectorAll('.adBlockNotificationOverlay,.adBlockNotification');
+    if (elems && elems.length) {
+        for (i = 0; i < elems.length; i++) {
+            elems[i].style = '';
+        }
+    }
+
+    console.warn('clean ads');
+    divsAdds = document.querySelectorAll('[id*=Ads_]');
+    if (divsAdds && divsAdds.length) {
+        for (i = 0; i < divsAdds.length; i++) {
+            console.log("removing");
+            divsAdds[i].remove();
+        }
     }
 }
 
-document.referrer = "";
+var observer = new MutationObserver(function () {
+    cleanAds();
+});
+
+observer.observe(
+    document, {
+        attributes: true,
+        childList: true,
+        characterData: true,
+        subtree: true
+    }
+);
